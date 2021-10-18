e356dc37c36fb2090e9ea26ad77ca208188f8ee9
Gerold Lee Gorman <38684198+glgorman@users.noreply.github.com>
2021-10-18 14:14:18 -0700

    Add files via upload

--- c++ interface/treesearch.cpp
+++ c++ interface/treesearch.cpp
@@ -13,21 +13,8 @@
 #include "../Frame Lisp/extras.h"
 #include "compiler.h"
 
-namespace chartype
-{
-	SET digits(10,'0','1','2','3','4','5','6','7','8','9');
-	SET whitespace(3,' ','\t','\n');
-	SET alpha(52,'A','B','C','D','E','F','G','H','I','J','K','L','M',
-		'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-		'a','b','c','d','e','f','g','h','i','j','k','l','m',
-		'n','o','p','q','r','s','t','u','v','w','x','y','z');
-	SET punct1(10,'.',',',':','\'','(',')','{','}','[',']');
-	SET operat(11,'+','-','*','/','%','|','~','=','&','>','<');
-}
-
-/* 
 // DEFINED IN COMPILER.H
-
+/*
 typedef enum _SYMBOL
 {
 	IDENT,COMMA,COLON,SEMICOLON,LPARENT,RPARENT,DOSY,TOSY,
